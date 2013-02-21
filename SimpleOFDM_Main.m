de4f1aeaae2689b61790aeebc6a0f25f80386d49
ktliao <liao1987@gmail.com>
2013-02-21 20:44:27 +0800

    set some system parameter

--- SimpleOFDM_Main.m
+++ SimpleOFDM_Main.m
@@ -1,4 +1,8 @@
 % Simple OFDM simulation 
 % Created by Ktliao, 20130221
+%% System parameter initialization
+%*Note
+SystemParams.samplingFrequency = 15e3;
+SystemParam.frequencySpacing = 15e3;
 
 
